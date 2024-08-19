0738833c34a0c79513b6c2e6c8dee4e9edaeeaf2
FauzanDiasKhairi <154121331+FauzanDias@users.noreply.github.com>
2024-08-19 20:44:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('simpan');
+
+if(simpan=25){
+    alert('Anda telah membeli budak silahkan tunggu 69 hari');
+} else if(simpan=18) {
+    alert('Anda telah membeli budak silahkan tunggu 69 hari');
+}
+
